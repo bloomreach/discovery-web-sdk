c736a4e999fd447c2cd4073639e5e60efb6bb2c9
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-04-16 21:54:00 -0700

    build(setup): Ignore vite.config in linting

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     lib: {
+      name: 'Bloomreach',
       entry: './src/index.ts',
-      name: 'DiscoveryWebSDK',
       fileName: 'index',
       formats: ['es'],
     },
