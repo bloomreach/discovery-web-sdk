9a59c4ce3d459b874276eefeeb46bdb4019df75b
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-04-22 11:10:09 -0700

    build(setup): Add 'msw' for mocking http responses in Vitest
    Vitest docs recommend using this package for http mocking. Having the option to mock these responses
    will allow us to create a set of integration tests to test the API surface and functionality of the
    SDK.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,21 @@
 import { describe, expect, test } from 'vitest';
-import { message } from './index';
+import { http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
+
+const handlers = [
+  http.get('https://core.dxpapi.com/api/v1/core/', () => {
+    return HttpResponse.json({
+      id: 'Hello world',
+    });
+  }),
+];
+
+const server = setupServer(...handlers);
+server.listen();
 
 describe('my test suite', () => {
-  test('say hello world', () => {
-    expect(message).toEqual('Hello world');
+  test('say hello world', async () => {
+    const response = await fetch('https://core.dxpapi.com/api/v1/core/').then((data) => data.json());
+    expect(response).toEqual({ id: 'Hello world' });
   });
 });
