c989596a874de33d31b51f2a670b5fce48f5744a
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-05-07 14:56:11 -0700

    build: Setup semantic release config

--- src/shared/types/configuration.type.ts
+++ src/shared/types/configuration.type.ts
@@ -2,9 +2,9 @@ import type { AccountParameters } from './account-params.type';
 import type { CatalogParameters } from './catalog-params.type';
 
 export interface SetupConfiguration {
-  searchEndpoint?: string;
   account_id: AccountParameters['account_id'];
-  auth_key?: AccountParameters['auth_key'];
   domain_key: CatalogParameters['domain_key'];
+  auth_key?: AccountParameters['auth_key'];
+  searchEndpoint?: string;
   view_id?: CatalogParameters['view_id'];
 }
