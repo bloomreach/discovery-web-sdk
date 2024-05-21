cc0137781b05de62574a524087492fe8f6cb732f
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-05-20 20:46:11 -0700

    feat(widgets): Add types and adjust to match with latest docs & openapi specs

--- src/features/autosuggest/suggest-request.type.ts
+++ src/features/autosuggest/suggest-request.type.ts
@@ -10,6 +10,7 @@ import type {
   url,
   user_id,
 } from '../../shared/parameters.type';
+import type { catalog_views, user_agent } from './suggest-parameters.type';
 
 export interface SuggestRequestParameters {
   _br_uid_2: _br_uid_2;
@@ -22,28 +23,6 @@ export interface SuggestRequestParameters {
   sort?: sort;
   url: url;
   user_id?: user_id;
-
-  /**
-   * A list of catalog views that you want to see in your suggestions. You must specify the catalog
-   * name within the catalog view. For Product catalogs, the catalog name is the same value as your
-   * domain_key.
-   *
-   * A catalog_views value contains the view_ids within a catalog separated by a colon. If you pass
-   * multiple catalogs in catalog_views, they must be pipe-separated. Attributes suggestions are
-   * enabled for the first two catalogs mentioned as part of catalog_views and only for the first
-   * view passed in each of the first two catalogs.
-   *
-   * For example, &catalog_views=product:store1,store2|recipe:premium|articles would return text and
-   * attribute suggestions for the product catalog in the store1 view, and for the recipe catalog in
-   * the premium view; only text suggestions will be displayed for the articles catalog.
-   */
-  catalog_views: string;
-
-  /**
-   * The user agent of the browser that's making the search request. Make sure that this value in your
-   * API request is the same as the value in the original request from your user. We optimize pages in
-   * part by crawling and gathering data, and the original user_agent lets us distinguish between our
-   * internal Bloomreach users who QA pages and everyone else who visits your site.
-   */
-  user_agent?: string;
+  catalog_views: catalog_views;
+  user_agent?: user_agent;
 }
---
+++ src/features/recs-pathways/widget-parameters.type.ts
@@ -0,0 +1,44 @@
+/**
+ * A comma-separated list of fields to be sent in the request.
+ *
+ * Alternatively, you may configure the fields in the Widget Configurator in the Dashboard instead. This parameter is required if not sent via the Dashboard.
+ */
+export type fields = string;
+
+/**
+ * The filter parameter applies a faceted filter to the returned products, searching for products that fit your parameter values. Any facet that you want to filter must be in your feed.
+ *
+ * Attributes must be enabled and mapped by Bloomreach. Let your Bloomreach representative know which attributes in your content feed you want to apply as filters to search results.
+ *
+ * You can filter results based on numeric ranges. For example, &filter=(price:["100" TO "*"]). Please note that a negative filter on ranges is not supported.
+ *
+ * To provide multiple filters, send multiple filter parameters. For example, &filter=(price:["*" TO "100"])&filter=(color_groups: ("blue"))
+ */
+export type filter = string;
+
+/**
+ * Your site's category ID.
+ *
+ * The example value shown here, pnb160000000000, is included for your convenience to send a request with Try It.
+ */
+export type cat_id = string;
+
+/**
+ * Facets are disabled by default. To enable facets, set facet=true.
+ */
+export type facet = boolean;
+
+/**
+ * Specifies access to a particular set of items. For product catalog, this would be the PID(s).
+ */
+export type item_ids = string[];
+
+/**
+ * Your site visitor's search query. Search queries are composed of one or more terms.
+ *
+ * You can percent encode spaces between terms as %20, or you can leave the spaces unencoded.
+ */
+export type query = string;
+
+export type cdp_segments = string;
+export type segment = string;
