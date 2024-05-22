59941a914aba334c5715813cf646657026154dcc
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-05-21 17:10:04 -0700

    refactor: Allow usage of `buildUrl` by all API types

--- src/shared/url-builders.ts
+++ src/shared/url-builders.ts
@@ -1,11 +1,6 @@
-import type { SuggestRequestParameters } from '../features/autosuggest/suggest-request.type';
-import type { SearchRequestParameters } from '../features/search/search-request.type';
-
 const shouldNotBeEncoded = ['_br_uid_2', 'fq', 'sort'];
 
-type RequestParameters = SearchRequestParameters | SuggestRequestParameters;
-
-export function buildApiUrl(base: string, params: RequestParameters): URL {
+export function buildApiUrl(base: string, params: Record<string, any>): URL {
   const url = new URL(base);
 
   url.search = Object.entries(params)
