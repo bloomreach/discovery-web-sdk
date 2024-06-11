00c81c62bfc2ab8e786a969e50c2a26279048bff
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-06-10 20:48:51 -0700

    fix: Duplicate JSDoc comments with explanation across interfaces
    The type aliasses prevented the JSDoc description to show up in auto complete in IDEs due to the
    extra type resolve step. The description is important to show during autocomplete so we will accept
    this duplication until we find a better solution.

--- src/features/autosuggest/suggest-request.type.ts
+++ src/features/autosuggest/suggest-request.type.ts
@@ -1,28 +1,69 @@
-import type {
-  _br_uid_2,
-  account_id,
-  callback,
-  q,
-  ref_url,
-  request_id,
-  request_type,
-  sort,
-  url,
-  user_id,
-} from '../../shared/parameters.type';
 import type { catalog_views, user_agent } from './suggest-parameters.type';
 
 export interface SuggestRequestParameters {
-  _br_uid_2: _br_uid_2;
-  account_id: account_id;
-  callback?: callback;
-  q: q;
-  ref_url?: ref_url;
-  request_id?: request_id;
-  request_type: request_type;
-  sort?: sort;
-  url: url;
-  user_id?: user_id;
+  /**
+   * First Party cookie that provides unique identifier
+   */
+  _br_uid_2: string;
+
+  /**
+   * Account ID
+   */
+  account_id: number;
+
+  /**
+   * Indicates whether data wrapped in the function for CORS requests
+   */
+  callback?: string;
+
+  /**
+   * Partial search query
+   */
+  q: string;
+
+  /**
+   * URL of the page or HTTP referrer where request started
+   */
+  ref_url?: string;
+
+  /**
+   * ID to track site visitor clicks
+   */
+  request_id?: number;
+
+  /**
+   * The type of API request. Value should be search for Product & Category Search.
+   * @default search
+   */
+  request_type: 'search' | 'suggest'
+
+  /**
+   * Sorts results based on the field value in ascending, descending, or another combination of orders.
+   * You can sort any fl field.
+   * Value is a field name, followed by asc/desc for ascending/descending order respectively.
+   */
+  sort?: string;
+
+  /**
+   * Absolute URL of the page where the request initiated
+   */
+  url: string;
+
+  /**
+   * The universal customer ID of the user. You only need to pass this field if your particular integration tracks customers this way.
+   * The parameter captures user IDs from the customer side, and reuses the information when powering apps or enhancing cross-device linking. In this way,
+   * Bloomreach recognizes users in a way that's aligned with your system. Use an anonymous string. Don't use email or other personally identifiable information.
+   * If you do not track users this way, then omit this field.
+   */
+  user_id?: string;
+
+  /**
+   * Product/Content catalog name
+   */
   catalog_views: catalog_views;
+
+  /**
+   * User agent string
+   */
   user_agent?: user_agent;
 }
--- src/shared/configuration.type.ts
+++ src/shared/configuration.type.ts
@@ -1,11 +1,38 @@
-import type { account_id, auth_key, domain_key, view_id } from './parameters.type';
-
 export interface Configuration {
-  account_id: account_id;
-  domain_key: domain_key;
-  auth_key?: auth_key;
+  /**
+   * Account ID
+   */
+  account_id: number;
+  /**
+   * Product/Content catalog name
+   */
+  domain_key: string;
+  /**
+   * Authentication Key
+   */
+  auth_key?: string;
+
+  /**
+   * The endpoint for Recommendations & Pathways Widgets API
+   */
   widgetEndpoint?: string;
+
+  /**
+   * The endpoint for Product Search & Category Search APIs
+   */
   searchEndpoint?: string;
+
+  /**
+   * The endpoint for Autosuggest API
+   */
   suggestEndpoint?: string;
-  view_id?: view_id;
+
+  /**
+   * A unique identifier for a specific view of your product catalog.
+   * If you have multiple versions of your site, each with their own product catalog characteristics like product titles and prices,
+   * then add view_id to your call.
+   * Bloomreach uses your view_id parameter value to display the right product information for your customers based on their individual site views.
+   * You can enter any string value to identify the specific site catalog view. This string must be consistent in your pixel, API, and product catalog.
+   */
+  view_id?: string;
 }
