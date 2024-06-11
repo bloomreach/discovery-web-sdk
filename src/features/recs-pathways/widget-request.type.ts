1ea1327f238499cdaebe164bb88c2c51b8ab0a4d
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-06-10 20:48:51 -0700

    fix: Duplicate JSDoc comments with explanation across interfaces
    The type aliasses prevented the JSDoc description to show up in auto complete in IDEs due to the
    extra type resolve step. The description is important to show during autocomplete so we will accept
    this duplication until we find a better solution.

--- src/features/recs-pathways/widget-request.type.ts
+++ src/features/recs-pathways/widget-request.type.ts
@@ -1,18 +1,4 @@
 import type {
-  _br_uid_2,
-  account_id,
-  auth_key,
-  domain_key,
-  ref_url,
-  request_id,
-  rows,
-  sort,
-  start,
-  url,
-  user_id,
-  view_id,
-} from '../../shared/parameters.type';
-import type {
   cat_id,
   facet,
   fields,
@@ -23,17 +9,70 @@ import type {
 } from './widget-parameters.type';
 
 export interface WidgetRequest {
-  _br_uid_2: _br_uid_2;
-  account_id: account_id;
-  auth_key?: auth_key;
-  domain_key: domain_key;
-  ref_url: ref_url;
-  request_id: request_id;
-  rows: rows;
-  sort?: sort;
-  start: start;
-  url: url;
-  view_id: view_id;
+  /**
+   * First Party cookie that provides unique identifier
+   */
+  _br_uid_2: string;
+
+  /**
+   * Account ID
+   */
+  account_id: number;
+
+  /**
+   * Authentication Key
+   */
+  auth_key?: string;
+
+  /**
+   * Product/Content catalog name
+   */
+  domain_key: string;
+
+  /**
+   * URL of the page or HTTP referrer where request started
+   */
+  ref_url: string;
+
+  /**
+   * ID to track site visitor clicks
+   */
+  request_id: number;
+
+  /**
+   * The number of matching items to return per results page in the API response.
+   * The maximum value is 200.
+   */
+  rows: number;
+
+  /**
+   * Sorts results based on the field value in ascending, descending, or another combination of orders.
+   * You can sort any fl field.
+   * Value is a field name, followed by asc/desc for ascending/descending order respectively.
+   */
+  sort?: string;
+
+  /**
+   * The number of the first item on a page of results.
+   * For example, the first item on the first page is 0, making the start value also 0.
+   * The maximum value is 10000.
+   */
+  start: number;
+
+  /**
+   * Absolute URL of the page where the request initiated
+   */
+  url: string;
+
+  /**
+   * A unique identifier for a specific view of your product catalog.
+   * If you have multiple versions of your site, each with their own product catalog characteristics like product titles and prices,
+   * then add view_id to your call.
+   * Bloomreach uses your view_id parameter value to display the right product information for your customers based on their individual site views.
+   * You can enter any string value to identify the specific site catalog view. This string must be consistent in your pixel, API, and product catalog.
+   */
+  view_id: string;
+
   segment?: segment;
   fields?: fields;
   filter?: filter;
@@ -53,13 +92,13 @@ export interface KeywordWidgetRequest extends WidgetRequest {
 
 export interface PersonalizedWidgetRequest extends WidgetRequest {
   query: query;
-  user_id: user_id;
+  user_id: string;
   facet?: facet;
 }
 
 export interface RecentlyViewedWidgetRequest extends WidgetRequest {
   query: query;
-  user_id?: user_id;
+  user_id?: string;
   facet?: facet;
 }
 
