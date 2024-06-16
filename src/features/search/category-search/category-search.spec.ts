3775234b63b964afcc8678548e26ebb78d5e274c
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-06-15 23:10:28 -0700

    test: Adjust tests so they actually fail on a failed expectation

--- src/features/search/category-search/category-search.spec.ts
+++ src/features/search/category-search/category-search.spec.ts
@@ -1,11 +1,11 @@
 import { faker } from '@faker-js/faker';
 import { HttpResponse, http } from 'msw';
 import { describe, expect, test } from 'vitest';
+import { createSetupConfigMock } from '../../../shared/configuration.mock';
 import { mockRequest } from '../../../shared/mock-request.mock';
 import { createSearchResponseMock } from '../search-response.mock';
 import { categorySearch } from './category-search';
 import { createCategorySearchOptionsMock } from './category-search-options.mock';
-import { createSetupConfigMock } from '../../../shared/configuration.mock';
 
 describe('Category Search API', () => {
   const config = createSetupConfigMock();
@@ -16,20 +16,21 @@ describe('Category Search API', () => {
   test('request and search type', async () => {
     const expectedRequestType = 'search';
     const expectedSearchType = 'category';
+    let searchParams: URLSearchParams;
 
     await mockRequest(
       categorySearch,
       [config, searchOptions],
       [
         http.get(config.searchEndpoint, ({ request }) => {
-          const { searchParams } = new URL(request.url);
-
-          expect(searchParams.get('request_type')).toEqual(expectedRequestType);
-          expect(searchParams.get('search_type')).toEqual(expectedSearchType);
-
+          searchParams = new URL(request.url).searchParams;
           return HttpResponse.json(createSearchResponseMock());
         }),
       ],
+      () => {
+        expect(searchParams.get('request_type')).toEqual(expectedRequestType);
+        expect(searchParams.get('search_type')).toEqual(expectedSearchType);
+      },
     );
   });
 });
