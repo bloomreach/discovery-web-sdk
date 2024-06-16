5cc7e20f8a7b9f6f1df03229c85f64e911a38bf7
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-06-15 22:14:56 -0700

    fix: Add more interfaces to the exported types such that consumers can use them

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-export type { AutosuggestOptions } from './features/autosuggest/autosuggest-options.type';
-export type { SuggestResponse } from './features/autosuggest/suggest-response.type';
-export type { BestsellerOptions } from './features/search/bestseller/bestseller-options.type';
-export type { CategorySearchOptions } from './features/search/category-search/category-search-options.type';
-export type { ContentSearchOptions } from './features/search/content-search/content-search-options.type';
-export type { ProductSearchOptions } from './features/search/product-search/product-search-options.type';
-export type { SearchResponse } from './features/search/search-response.type';
-export type { Configuration } from './shared/configuration.type';
-export type { WidgetResponse } from './features/recs-pathways/widget-response.type';
-export type * from './features/recs-pathways/widget-options.type';
 export { autoSuggest } from './features/autosuggest/autosuggest';
+export type { AutosuggestOptions } from './features/autosuggest/autosuggest-options.type';
+export type * from './features/autosuggest/suggest-response.type';
+export type * from './features/recs-pathways/widget-options.type';
+export type * from './features/recs-pathways/widget-response.type';
+export {
+  getCategoryWidget,
+  getGlobalWidget,
+  getItemWidget,
+  getKeywordWidget,
+  getPersonalizedWidget,
+  getRecentlyViewedWidget,
+} from './features/recs-pathways/widgets';
 export { bestseller } from './features/search/bestseller/bestseller';
+export type { BestsellerOptions } from './features/search/bestseller/bestseller-options.type';
 export { categorySearch } from './features/search/category-search/category-search';
+export type { CategorySearchOptions } from './features/search/category-search/category-search-options.type';
 export { contentSearch } from './features/search/content-search/content-search';
+export type { ContentSearchOptions } from './features/search/content-search/content-search-options.type';
 export { productSearch } from './features/search/product-search/product-search';
-export * from './features/recs-pathways/widgets';
+export type { ProductSearchOptions } from './features/search/product-search/product-search-options.type';
+export type * from './features/search/search-response.type';
+export type { Configuration } from './shared/configuration.type';
