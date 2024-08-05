d04311c8fd1c421642ed4ee3d651c353ceefb824
Anand Gorantala <anand.gorantala@bloomreach.com>
2024-08-05 15:18:54 -0700

    feat: Add support for multiple values for params

--- src/shared/url-builders.ts
+++ src/shared/url-builders.ts
@@ -1,4 +1,12 @@
-const shouldNotBeEncoded = ['_br_uid_2', 'fq'];
+const shouldNotBeEncoded = ['_br_uid_2'];
+
+function getValue(key: string, value: string | number | boolean | Array<string | number | boolean>): string {
+  if (Array.isArray(value)) {
+    return value.map((v: string | number | boolean) => getValue(key, v)).join('&');
+  }
+
+  return `${key}=${shouldNotBeEncoded.includes(key) ? value : encodeURIComponent(value)}`;
+}
 
 export function buildApiUrl<T extends object>(base: string, params: T): URL {
   const url = new URL(base);
@@ -6,7 +14,7 @@ export function buildApiUrl<T extends object>(base: string, params: T): URL {
   url.search = Object.entries(params)
     .filter(([, value]) => value !== undefined)
     .map(([key, value]) => {
-      return `${key}=${shouldNotBeEncoded.includes(key) ? value : encodeURIComponent(value as string | number | boolean)}`;
+      return getValue(key, value as string | number | boolean | Array<string | number | boolean>);
     })
     .join('&');
 
