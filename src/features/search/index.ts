d88093903d147d0581a9e62a64ca71d0d68cff36
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-09-10 12:03:34 -0700

    fix: Add exports of run time checks for Options objects
    Opted for using barrel exports per feature to simplify overview of API

---
+++ src/features/search/index.ts
@@ -0,0 +1,14 @@
+export { bestseller, isBestsellerOptions } from './bestseller/bestseller';
+export type { BestsellerOptions } from './bestseller/bestseller-options.type';
+
+export { categorySearch, isCategorySearchOptions } from './category-search/category-search';
+export type { CategorySearchOptions } from './category-search/category-search-options.type';
+
+export { contentSearch, isContentSearchOptions } from './content-search/content-search';
+export type { ContentSearchOptions } from './content-search/content-search-options.type';
+
+export { productSearch, isProductSearchOptions } from './product-search/product-search';
+export type { ProductSearchOptions } from './product-search/product-search-options.type';
+
+export type * from './search-response.type';
+
