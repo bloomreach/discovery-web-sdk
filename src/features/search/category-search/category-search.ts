dacfad9801affa1abe831f62c6f4d4866679177a
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2024-09-23 16:36:53 -0700

    fix: Reject API calls that return a HTTP status that is not 2xx
    This should allow consumers to properly handle any 400 / 500 http errors

--- src/features/search/category-search/category-search.ts
+++ src/features/search/category-search/category-search.ts
@@ -1,6 +1,7 @@
 import type { Configuration } from '../../../shared/configuration.type';
 import { SEARCH_ENDPOINT_PROD } from '../../../shared/constants';
 import { logAPICall } from '../../../shared/logger';
+import { typedFetch } from '../../../shared/typed-fetch';
 import { buildApiUrl } from '../../../shared/url-builders';
 import type { SearchRequestParameters } from '../search-request.type';
 import type { SearchResponse } from '../search-response.type';
@@ -42,6 +43,5 @@ export async function categorySearch(
 
   logAPICall('categorySearch', configuration, options, FIXED_OPTIONS, defaults, queryParams, url);
 
-  const data = await fetch(url);
-  return data.json() as Promise<SearchResponse>;
+  return typedFetch<SearchResponse>(url);
 }
